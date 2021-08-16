49265d8ee1d247fbefee6d1c1eaf649f0ee249cf
Ivoneide Duarte <67153931+Ivoneideduarte@users.noreply.github.com>
2021-08-16 10:58:04 -0300

    update

--- Program/positio_servants/positio_servants.ino
+++ Program/positio_servants/positio_servants.ino
@@ -1,50 +1,50 @@
 #include <Servo.h>
 
 #define potBase A0
-#define potGarra A1
-#define potAvanco A2
-#define potElevacao A3
+#define potAvanco A1
+#define potElevacao A2
+#define potGarra A3
 
 Servo servoBase;
-Servo servoGarra;
 Servo servoAvanco;
 Servo servoElevacao;
+Servo servoGarra;
 
 int anguloBase;
-int anguloElevacao;
 int anguloAvanco;
+int anguloElevacao;
 int anguloGarra;
 
 int tmp = 1000;
 
-void setup() 
+void setup()
 {
   servoBase.attach(2);
-  servoGarra.attach(3);
-  servoAvanco.attach(4);
-  servoElevacao.attach(5);
-  
+  servoAvanco.attach(3);
+  servoElevacao.attach(4);
+  servoGarra.attach(5);
+
   Serial.begin(9600);
 }
 
-void loop() 
+void loop()
 {
   anguloBase = analogRead(potBase);
   anguloBase = map(anguloBase, 0, 1023, 0, 180);
   servoBase.write(anguloBase);
-  
-  anguloElevacao = analogRead(potElevacao);
-  anguloElevacao = map(anguloElevacao, 0, 1023, 0, 180);
-  servoElevacao.write(anguloElevacao);
 
   anguloAvanco = analogRead(potAvanco);
   anguloAvanco = map(anguloAvanco, 0, 1023, 0, 180);
   servoAvanco.write(anguloAvanco);
-  
+
+  anguloElevacao = analogRead(potElevacao);
+  anguloElevacao = map(anguloElevacao, 0, 1023, 0, 180);
+  servoElevacao.write(anguloElevacao);
+
   anguloGarra = analogRead(potGarra);
   anguloGarra = map(anguloGarra, 0, 1023, 0, 180);
   servoGarra.write(anguloGarra);
-  
+
   servosPos();
 }
 
@@ -53,11 +53,11 @@ void servosPos()
   Serial.println("--------------------------------------------------");
   Serial.print(servoBase.read());
   Serial.print(" - ");
-  Serial.print(servoGarra.read());
-  Serial.print(" - ");
   Serial.print(servoAvanco.read());
   Serial.print(" - ");
-  Serial.println(servoElevacao.read());
+  Serial.print(servoElevacao.read());
+  Serial.print(" - ");
+  Serial.println(servoGarra.read());
   Serial.println("--------------------------------------------------");
 }
 
